9af6eeca642d0f08c3d50a415818c0816af56ea6
sangam <sangampokharel803@gmail.com>
2025-05-14 15:42:39 +0545

    Initial Commit

---
+++ ImageClassificationDemo/ImageClassificationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassificationDemoApp.swift
+//  ImageClassificationDemo
+//
+//  Created by EKbana on 14/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ImageClassificationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
